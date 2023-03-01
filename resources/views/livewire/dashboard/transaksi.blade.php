659f6ab1e2003228ade5e906c5b7250b83cad973
faridaj2 <42547920+faridaj2@users.noreply.github.com>
2023-03-01 08:15:29 +0700

    01 maret 2023

--- resources/views/livewire/dashboard/transaksi.blade.php
+++ resources/views/livewire/dashboard/transaksi.blade.php
@@ -1,10 +1,10 @@
 <div>
-    <div class="col-span-12 mt-6" x-data="{nama: 'Oke'}">
+    <div class="col-span-12 mt-6" x-data="{ nama: 'Oke' }">
         {{-- Form --}}
         <div x-data="{ open: false }">
             <button @click=" open = ! open" x-show="!open" class="btn btn-elevated-rounded-primary w-24 mr-1 mb-2">New
                 Pay</button>
-            <div class="intro-y col-span-12 lg:col-span-6" x-show="open" @click.outside="open = false">
+            <div class="intro-y col-span-12 lg:col-span-6" x-show="open">
                 <div class="block flex justify-end">
                     <button @click=" open = ! open" class="btn btn-elevated-rounded-secondary w-24 mr-1 mb-2">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
@@ -18,7 +18,7 @@
                 <div class="intro-y box p-5">
                     <div>
                         <label for="crud-form-1" class="form-label">Nama Pembayaran</label>
-                        <input id="crud-form-1" type="text" class="form-control w-full"
+                        <input id="crud-form-1" type="text" class="form-control w-full outline-none border-slate-200"
                             placeholder="Nama Pembayaran">
                     </div>
                     <div class="mt-3">
@@ -54,6 +54,13 @@
                         </select>
 
                     </div>
+                    <div class="mt-3">
+                        
+                                    
+                        <input type="text" class="datepicker form-control w-56 inline-block border-slate-200 rounded-md" data-single-mode="true">
+                                    
+                                
+                    </div>
                     {{-- <div class="mt-3">
                         <label>Active Status</label>
                         <div class="form-switch mt-2">
@@ -77,7 +84,7 @@
                         <div class="p-5 text-center">
                             <i data-lucide="x-circle" class="w-16 h-16 text-danger mx-auto mt-3"></i>
                             <div class="text-3xl mt-5">Apa kamu yakin?</div>
-                            <div class="text-slate-500 mt-2" >Apa kamu akan menghapus <span x-text="nama"></span></div>
+                            <div class="text-slate-500 mt-2">Apa kamu akan menghapus <span x-text="nama"></span></div>
                         </div>
                         <div class="px-5 pb-8 text-center">
                             <button type="button" data-tw-dismiss="modal"
@@ -93,19 +100,20 @@
         {{-- Table --}}
         <div class="intro-y col-span-12 overflow-auto lg:overflow-visible">
 
-            <table class="table table-report -mt-2">
+            <table class="table table-report -mt-2" x-data="{tab: 0}">
                 <thead>
                     <tr>
                         <th class="whitespace-nowrap">#</th>
                         <th class="whitespace-nowrap">Pembayaran</th>
                         <th class="whitespace-nowrap">Total</th>
                         <th class="text-center whitespace-nowrap">Status</th>
+                        <th>Tanggal</th>
                         <th class="text-center whitespace-nowrap">Actions</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr class="intro-x">
-                        <td class="w-40">
+                        <td class="w-40" colspan="">
                             1
 
                         </td>
@@ -125,57 +133,12 @@
                                 </svg> Keluar
                             </div>
                         </td>
-                        <td class="table-report__action w-56">
-                            <div class="flex justify-center items-center">
-                                <a class="flex items-center mr-3" href="javascript:;">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                        viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                        stroke-linecap="round" stroke-linejoin="round" icon-name="check-square"
-                                        data-lucide="check-square" class="lucide lucide-check-square w-4 h-4 mr-1">
-                                        <polyline points="9 11 12 14 22 4"></polyline>
-                                        <path d="M21 12v7a2 2 0 01-2 2H5a2 2 0 01-2-2V5a2 2 0 012-2h11"></path>
-                                    </svg> Edit
-                                </a>
-                                <a href="javascript:;" data-tw-toggle="modal" data-tw-target="#delete-modal-preview" @click="nama = nama = 'Anjali'" class="flex items-center text-danger" href="javascript:;" data-tw-toggle="modal"
-                                    data-tw-target="#delete-confirmation-modal">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                        viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                        stroke-linecap="round" stroke-linejoin="round" icon-name="trash-2"
-                                        data-lucide="trash-2" class="lucide lucide-trash-2 w-4 h-4 mr-1">
-                                        <polyline points="3 6 5 6 21 6"></polyline>
-                                        <path
-                                            d="M19 6v14a2 2 0 01-2 2H7a2 2 0 01-2-2V6m3 0V4a2 2 0 012-2h4a2 2 0 012 2v2">
-                                        </path>
-                                        <line x1="10" y1="11" x2="10" y2="17"></line>
-                                        <line x1="14" y1="11" x2="14" y2="17"></line>
-                                    </svg> Delete
-                                </a>
-                            </div>
-                        </td>
-                    </tr>
-                    <tr class="intro-x">
-                        <td class="w-40">
-                            2
-                        </td>
                         <td>
-                            <a href="" class="font-medium whitespace-nowrap">Apple MacBook Pro 13</a>
-                            <div class="text-slate-500 text-xs whitespace-nowrap mt-0.5">PC &amp; Laptop</div>
-                        </td>
-                        <td>Rp. 50000</td>
-                        <td class="w-40">
-                            <div class="flex items-center justify-center text-success">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                    viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                    stroke-linecap="round" stroke-linejoin="round" icon-name="check-square"
-                                    data-lucide="check-square" class="lucide lucide-check-square w-4 h-4 mr-2">
-                                    <polyline points="9 11 12 14 22 4"></polyline>
-                                    <path d="M21 12v7a2 2 0 01-2 2H5a2 2 0 01-2-2V5a2 2 0 012-2h11"></path>
-                                </svg> Masuk
-                            </div>
+                            05 February 2015
                         </td>
                         <td class="table-report__action w-56">
                             <div class="flex justify-center items-center">
-                                <a class="flex items-center mr-3" href="javascript:;">
+                                <a @click="tab = (tab === '0' ? '1' : '0' )"  class="flex items-center mr-3" href="javascript:;" @click="edit = true">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                                         viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
                                         stroke-linecap="round" stroke-linejoin="round" icon-name="check-square"
@@ -184,7 +147,8 @@
                                         <path d="M21 12v7a2 2 0 01-2 2H5a2 2 0 01-2-2V5a2 2 0 012-2h11"></path>
                                     </svg> Edit
                                 </a>
-                                <a href="javascript:;" data-tw-toggle="modal" data-tw-target="#delete-modal-preview" @click="nama = nama = 'Farid'" class="flex items-center text-danger"
+                                <a href="javascript:;" data-tw-toggle="modal" data-tw-target="#delete-modal-preview"
+                                    @click="nama = nama = 'Anjali'" class="flex items-center text-danger"
                                     href="javascript:;" data-tw-toggle="modal"
                                     data-tw-target="#delete-confirmation-modal">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
@@ -201,6 +165,137 @@
                                 </a>
                             </div>
                         </td>
+
+                    </tr>
+                    <tr x-show="tab === 1">
+                        <form action="">
+                            <td>
+                                Edit
+                            </td>
+                            <td>
+                                <input type="text" class="p-1 rounded-md mb-1 border-slate-300">
+                                <input type="text" class="p-1 rounded-md border-slate-300">
+                            </td>
+                            <td>
+                                <input type="text" class="p-1 rounded-md border-slate-300">
+                            </td>
+                            <td>
+                                <select name="" id="" class="p-1 pr-8 rounded-md border-slate-300">
+                                    <option value="">Keluar</option>
+                                    <option value="">Masuk</option>
+                                </select>
+                            </td>
+                            <td>
+                                
+                                    
+                                <input type="text" class="datepicker w-20 border-slate-200 rounded-md" data-single-mode="true">
+                                    
+                                
+                            <td class="table-report__action w-56">
+                                <div class="flex justify-center items-center">
+                                    <a class="flex items-center mr-3" href="javascript:;" @click="edit = true">
+                                         Simpan
+                                    </a>
+                                    <a href="javascript:;" class="flex items-center text-danger"
+                                        @click="tab = 0">
+                                         Batal
+                                    </a>
+                                </div>
+                            </td>
+                        </form>
+                        
+                    </tr>
+                    <tr class="intro-x">
+                        <td class="w-40">
+                            2
+                        </td>
+                        <td>
+                            <a href="" class="font-medium whitespace-nowrap">Apple MacBook Pro 13</a>
+                            <div class="text-slate-500 text-xs whitespace-nowrap mt-0.5">PC &amp; Laptop</div>
+                        </td>
+                        <td>Rp. 50s000</td>
+                        <td class="w-40">
+                            <div class="flex items-center justify-center text-success">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                    viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                    stroke-linecap="round" stroke-linejoin="round" icon-name="check-square"
+                                    data-lucide="check-square" class="lucide lucide-check-square w-4 h-4 mr-2">
+                                    <polyline points="9 11 12 14 22 4"></polyline>
+                                    <path d="M21 12v7a2 2 0 01-2 2H5a2 2 0 01-2-2V5a2 2 0 012-2h11"></path>
+                                </svg> Masuk
+                            </div>
+                        </td>
+                        <td>
+                            10 january 2015
+                        </td>
+                        <td class="table-report__action w-56">
+                            <div class="flex justify-center items-center">
+                                <a @click="tab = 2" class="flex items-center mr-3" href="javascript:;">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                        viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                        stroke-linecap="round" stroke-linejoin="round" icon-name="check-square"
+                                        data-lucide="check-square" class="lucide lucide-check-square w-4 h-4 mr-1">
+                                        <polyline points="9 11 12 14 22 4"></polyline>
+                                        <path d="M21 12v7a2 2 0 01-2 2H5a2 2 0 01-2-2V5a2 2 0 012-2h11"></path>
+                                    </svg> Edit
+                                </a>
+                                <a href="javascript:;" data-tw-toggle="modal" data-tw-target="#delete-modal-preview"
+                                    @click="nama = nama = 'Farid'" class="flex items-center text-danger"
+                                    href="javascript:;" data-tw-toggle="modal"
+                                    data-tw-target="#delete-confirmation-modal">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                        viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                        stroke-linecap="round" stroke-linejoin="round" icon-name="trash-2"
+                                        data-lucide="trash-2" class="lucide lucide-trash-2 w-4 h-4 mr-1">
+                                        <polyline points="3 6 5 6 21 6"></polyline>
+                                        <path
+                                            d="M19 6v14a2 2 0 01-2 2H7a2 2 0 01-2-2V6m3 0V4a2 2 0 012-2h4a2 2 0 012 2v2">
+                                        </path>
+                                        <line x1="10" y1="11" x2="10" y2="17"></line>
+                                        <line x1="14" y1="11" x2="14" y2="17"></line>
+                                    </svg> Delete
+                                </a>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr x-show="tab === 2" @click.outside="tab = 0">
+                        <form action="">
+                            <td>
+                                Edit
+                            </td>
+                            <td>
+                                <input type="text" class="p-1 rounded-md mb-1 border-slate-300">
+                                <input type="text" class="p-1 rounded-md border-slate-300">
+                            </td>
+                            <td>
+                                <input type="text" class="p-1 rounded-md border-slate-300">
+                            </td>
+                            <td>
+                                <select name="" id="" class="p-1 pr-8 rounded-md border-slate-300">
+                                    <option value="">Keluar</option>
+                                    <option value="">Masuk</option>
+                                </select>
+                            </td>
+                            <td>
+                                
+                                    
+                                <input type="text" class="datepicker w-20 border-slate-200 rounded-md" data-single-mode="true">
+                                    
+                                
+                            </td>
+                            <td class="table-report__action w-56">
+                                <div class="flex justify-center items-center">
+                                    <a class="flex items-center mr-3" href="javascript:;" @click="edit = true">
+                                         Simpan
+                                    </a>
+                                    <a href="javascript:;" class="flex items-center text-danger"
+                                        @click="tab = 0">
+                                         Batal
+                                    </a>
+                                </div>
+                            </td>
+                        </form>
+                        
                     </tr>
 
                 </tbody>
